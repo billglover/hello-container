a5ee6c3631f2e588c1188fbdfb15602f3415a890
Bill Glover <bglover@pivotal.io>
2019-10-31 13:15:45 +0000

    Add webserver to display build version

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM scratch
 COPY hello /
+COPY ./cmd/hello/index.html /
 ENTRYPOINT ["/hello"]
--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
 
 var (
 	version = "dev"
@@ -8,6 +13,49 @@ var (
 	date    = "unknown"
 )
 
+// pageInfo holds all the data used to render the index page.
+type pageInfo struct {
+	Version  string
+	Commit   string
+	Date     string
+	Hostname string
+}
+
 func main() {
-	fmt.Printf("%v, commit %v, built at %v\n", version, commit, date)
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		hostname, err := os.Hostname()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		data := pageInfo{
+			Version:  version,
+			Commit:   commit,
+			Date:     date,
+			Hostname: hostname,
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		return
+	})
+
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
