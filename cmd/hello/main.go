8280145ba327afe42cea1144448abce6f039f46d
Bill Glover <bglover@pivotal.io>
2019-11-07 10:35:53 +0000

    Fix #2: allow user to terminate server by calling /kill

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -21,6 +22,13 @@ type pageInfo struct {
 	Hostname string
 }
 
+// Server holds configuration details for the HTTP server.
+type server struct {
+	cancel func()
+	tmpl   *template.Template
+	s      http.Server
+}
+
 func main() {
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
@@ -28,7 +36,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	m := http.NewServeMux()
+	s := server{cancel: cancel, tmpl: tmpl, s: http.Server{Addr: ":8080", Handler: m}}
+
+	m.HandleFunc("/", s.info())
+	m.HandleFunc("/kill", s.kill(s.info()))
+	m.HandleFunc("/health", s.health())
+
+	go func() {
+		if err := s.s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		s.s.Shutdown(ctx)
+	}
+	fmt.Fprintln(os.Stdout, "application terminated by user")
+}
+
+func (s *server) info() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-cache")
 
 		hostname, err := os.Hostname()
@@ -44,20 +77,22 @@ func main() {
 			Hostname: hostname,
 		}
 
-		err = tmpl.Execute(w, data)
+		err = s.tmpl.Execute(w, data)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	})
+	}
+}
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		return
-	})
+func (s *server) health() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
 
-	err = http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+func (s *server) kill(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h(w, r)
+		defer s.cancel()
 	}
 }
